ddf2e8aa68237b78a2514733ea87535b13eec7d0
ryoutanishimura2020-hue <ryouta.nishimura.2020@globis.ac.jp>
2025-08-27 01:09:33 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -536,14 +536,7 @@ export default function LandingPage() {
                   チケット購入
                </a>
                 
-                <a href="https://mba.globis.ac.jp/inquiry/#yokohama" 
-                 target="_blank" 
-                 rel="noopener noreferrer" 
-                 className="bg-yellow-400 text-black font-bold px-4 py-2 rounded /*cursor-not-allowed*/" 
-                 /*disabled*/>
-                  セッション申し込み
-               </a>
-                
+
                 {/*チケット完売後のボタン表示用
                 <button 
                   className="bg-gray-400 text-black font-bold px-4 py-2 rounded /*cursor-not-allowed*/" /*disabled*/>
