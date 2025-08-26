d2c962b3b16cf232efb660ada1250f4bb256fe93
ryoutanishimura2020-hue <ryouta.nishimura.2020@globis.ac.jp>
2025-08-26 23:22:46 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'かもめ会議2024',
-  description: 'かもめ会議 2024 - 新たな視点で、ビジネスの未来を描く',
+  title: 'かもめ会議2025',
+  description: 'かもめ会議 2025 - 新たな視点で、ビジネスの未来を描く',
 }
 
 export default function RootLayout({
