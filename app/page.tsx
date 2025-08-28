30117b4e6bc11a9030a04460ce10acf45fd1430d
ryoutanishimura2020-hue <ryouta.nishimura.2020@globis.ac.jp>
2025-08-28 20:03:52 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,7 @@ const HeroAnimation = () => {
 
       {/* Top overlay - static */}
       <img
-        src="/kamome_logo_padding.png"
+        src="/kamome_logo.png"
         alt=""
         className="absolute inset-0 w-full h-full object-contain"
         aria-hidden="true"
