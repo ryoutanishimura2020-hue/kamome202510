5f975e7bdfdd8dbee5dd6092071173600514ed43
ryoutanishimura2020-hue <ryouta.nishimura.2020@globis.ac.jp>
2025-09-01 23:41:02 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,7 @@ const HeroAnimation = () => {
       <img
         src="/kamome_logo_padding.png"
         alt=""
-        className="absolute items-center inset-0 w-3/4 h-auto object-contain"
+        className="absolute items-center w-3/4 h-auto object-contain"
         aria-hidden="true"
       />
     </div>
